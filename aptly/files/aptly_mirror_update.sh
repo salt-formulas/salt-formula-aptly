7808a7fd8ed1633084ffdae4a048e1fe7e9f5b9e
Richard Felkl <rfelkl@mirantis.com>
2017-11-15 18:01:46 +0100

    New Aptly scripts
    Change-Id: I628a9341da02c28f33a84fc5b1da3cbbd7e72ea4

--- aptly/files/aptly_mirror_update.sh
+++ aptly/files/aptly_mirror_update.sh
@@ -1,71 +1,90 @@
 #!/bin/bash
-
+try=0
+retval=666
 SCRIPT=$(basename $0)
 MAXTRIES=3
 VERBOSE=0
 SNAPSHOT=0
+MIRROR=""
 
 log_info() {
-	logger -p user.info -t ${SCRIPT} "$*"
-	[ $VERBOSE -eq 1 ] && echo "[INFO] $*"
+    logger -p user.info -t ${SCRIPT} "$*"
+    [ $VERBOSE -eq 1 ] && echo "[INFO] $*"
 }
 
 log_error() {
-	logger -p user.error -t ${SCRIPT} "$*"
-	echo "[ERROR] $*" >&2
+    logger -p user.error -t ${SCRIPT} "$*"
+    echo "[ERROR] $*" >&2
 }
 
-if [[ "$*" == *--verbose* || "$*" == *-v* ]]; then
-	VERBOSE=1
-fi
+update_mirror() {
+    if [[ "$1" == "" ]]; then
+        log_error "Mirror has to be specified."
+        exit 3
+    fi
+    while [[ $retval -ne 0 && $try -lt $MAXTRIES ]]; do
+        log_info "Starting update of mirror ${1}"
+        if [[ $VERBOSE -eq 0 ]]; then
+            out=$(aptly mirror update -force=true ${1} >/dev/null 2>&1)
+        else
+            aptly mirror update -force=true ${1}
+        fi
 
-if [[ "$*" == *--snapshot* || "$*" == *-s* ]]; then
-	SNAPSHOT=1
-fi
+        retval=$?
 
-MIRRORS=$(aptly mirror list --raw 2>&1)
-if [[ $? -ne 0 ]]; then
-	log_error "$MIRRORS"
-	exit 1
-fi
+        if [[ $retval -ne 0 ]]; then
+            try=$[ $try + 1 ]
+            log_error "Failed to update mirror ${1}, try=${try}"
+            [ ! -z "$out" ] && log_error "${out}"
+        else
+            log_info "Synced mirror ${1}"
+            if [[ $SNAPSHOT -eq 1 ]]; then
+                snapshot_name="${1}-$(date +%s)"
+                if [[ $VERBOSE -eq 0 ]]; then
+                    out=$(aptly snapshot create ${snapshot_name} from mirror ${1} >/dev/null 2>&1)
+                else
+                    aptly snapshot create ${snapshot_name} from mirror ${1}
+                fi
 
-for mirror in $MIRRORS; do
-	try=0
-	retval=666
+                retval=$?
+                if [[ $retval -ne 0 ]]; then
+                    log_error "Failed to create snapshot ${snapshot_name} from mirror ${1}"
+                    [ ! -z "$out" ] && log_error "$out"
+                else
+                    log_info "Created snapshot ${snapshot_name} from mirror ${1}"
+                fi
+            fi
+            break
+        fi
+    done
+    try=0
+    retval=666
+}
 
-	while [[ $retval -ne 0 && $try -lt $MAXTRIES ]]; do
-		log_info "Starting update of mirror ${mirror}"
-		if [[ $VERBOSE -eq 0 ]]; then
-			out=$(aptly mirror update -force=true ${mirror} >/dev/null 2>&1)
-		else
-			aptly mirror update -force=true ${mirror}
-		fi
-
-		retval=$?
-
-		if [[ $retval -ne 0 ]]; then
-			try=$[ $try + 1 ]
-			log_error "Failed to update mirror ${mirror}, try=${try}"
-			[ ! -z "$out" ] && log_error "${out}"
-		else
-			log_info "Synced mirror ${mirror}"
-			if [[ $SNAPSHOT -eq 1 ]]; then
-				snapshot_name="${mirror}-$(date +%s)"
-				if [[ $VERBOSE -eq 0 ]]; then
-					out=$(aptly snapshot create ${snapshot_name} from mirror ${mirror} >/dev/null 2>&1)
-				else
-					aptly snapshot create ${snapshot_name} from mirror ${mirror}
-				fi
-
-				retval=$?
-				if [[ $retval -ne 0 ]]; then
-					log_error "Failed to create snapshot ${snapshot_name} from mirror ${mirror}"
-					[ ! -z "$out" ] && log_error "$out"
-				else
-					log_info "Created snapshot ${snapshot_name} from mirror ${mirror}"
-				fi
-			fi
-			break
-		fi
-	done
+while getopts "m:s?v?"  option
+do
+ case "${option}"
+ in
+ m|\?) MIRROR=${OPTARG};;
+ s|\?) SNAPSHOT=1;;
+ v|\?) VERBOSE=1;;
+ esac
 done
+
+if [[ "$MIRROR" != "" ]]; then
+    MIRROR_DETAIL=$(aptly mirror show ${MIRROR})
+    if [[ $? -ne 0 ]]; then
+        log_error "$MIRROR_DETAIL"
+        exit 1
+    fi
+    update_mirror "${MIRROR}"
+else
+    MIRRORS=$(aptly mirror list --raw 2>&1)
+    if [[ $? -ne 0 ]]; then
+        log_error "$MIRRORS"
+        exit 1
+    fi
+    for mirror in $MIRRORS; do
+        update_mirror "${mirror}"
+    done
+fi
